5b44f305b04d56ae7e2e648359d7f828d9588f65
Shashank Sharma <shashankxdev@gmail.com>
2026-04-16 21:21:02 +0530

    Initial project setup with model classes

---
+++ src/main/java/com/library/model/Book.java
@@ -0,0 +1,28 @@
+package com.library.model;
+
+public class Book {
+    private int id;
+    private  String title;
+    private String author;
+    private boolean isAvailable;
+
+    public book(int id, String title, String author ) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+     }
+
+    public void borrowBook() {
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println("Book borrowed");
+        } else  {
+            System.out.println("Not available");
+        }
+    }
+
+    public void returnBook()  {
+        isAvailable = true;
+    }
+}
